72e4dd1f076220e4cfc27579ad212b6355f43053
rootPC\root <kbssxxx@gmail.com>
2017-09-07 00:58:16 +0300

    Старт проекта Редактор: 1.Реализованы блоки для сетки       1.1 Очень кривой спрайт       1.2 Не реализован механизм клика 2.Реализован механизм создания сетки из блоков 3. Реализован обьект резистор с механизмом клика       3.1 Еще более кривой спрайт

---
+++ Assets/Scripts/BoardManager.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System;
+using UnityEngine;
+
+public class BoardManager : MonoBehaviour
+{
+    [SerializeField]
+    public int columns = 6;
+    [SerializeField]
+    public int rows = 1;
+
+    public GameObject floorTiles;
+    public static GameObject toInstantiate;
+    private Transform boardHolder;
+    private List<Vector3> gridPositions = new List<Vector3>();
+
+    void InitialiseList()
+    {
+        gridPositions.Clear();
+        for (int x = -12; x < columns; x++)
+        {
+            for (int y = -3; y < rows; y++)
+            {
+                gridPositions.Add(new Vector3(x, y, 0.0f));
+            }
+        }
+
+    }
+
+  void BoardSetup()
+    {
+        boardHolder = new GameObject("Board").transform;
+        for (int x = -14; x < columns; x++)
+        {
+            for (int y = -3; y < rows; y++)
+            {
+
+                GameObject inctance = Instantiate(floorTiles, new Vector3(x, y, 0f), Quaternion.identity) as GameObject;
+
+                inctance.transform.SetParent(boardHolder);
+            }
+        }
+    }
+
+
+    public void SetupScene()
+    {
+        BoardSetup();
+        InitialiseList();
+    }
+}
---
+++ Assets/Scripts/TilesScript.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TilesScript : MonoBehaviour {
+
+    private void OnMouseDown()
+    {
+        BoardManager.toInstantiate = gameObject;
+    }
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+
+}
